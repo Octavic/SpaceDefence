c19c23dc30ed07c8cbe5a4e9a2c29b5ee691b379
Octavic <yifeixu@live.com>
2018-01-22 08:58:00 -0800

    Updated the sprites to fit the size

--- SpaceDefense/Assets/Wiring/Weapon/Projectile.cs
+++ SpaceDefense/Assets/Wiring/Weapon/Projectile.cs
@@ -1,5 +1,5 @@
 ﻿//  --------------------------------------------------------------------------------------------------------------------
-//  <copyright file="GameController.cs">
+//  <copyright file="Projectile.cs">
 //    Copyright (c) Yifei Xu .  All rights reserved.
 //  </copyright>
 //  --------------------------------------------------------------------------------------------------------------------
@@ -15,12 +15,25 @@ namespace Assets.Wiring.Weapon
     /// <summary>
     /// Defines a projectile
     /// </summary>
-    public interface IProjectile
+    public class Projectile : MonoBehaviour
     {
+        /// <summary>
+        /// the effect that's carried
+        /// </summary>
+        public EffectEnum CarriedEffect;
+
+        /// <summary>
+        /// Gets or sets the damage for the projectile
+        /// </summary>
+        public float Damage;
+
         /// <summary>
         /// Called when the projectile hits an enemy
         /// </summary>
-        /// <param name="hitEnemy">The enmy that was hit</param>
-        void OnHittingEnemy(Enemy hitEnemy);
+        /// <param name="hitEnemy">The enemy that was hit</param>
+        public void OnHittingEnemy(Enemy hitEnemy)
+        {
+            hitEnemy.TakeDamage(this.Damage, this.CarriedEffect);
+        }
     }
 }
