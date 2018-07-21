56157e88c60fe09ca62dbe78085c631633dc1a9e
Octavic <yifeixu@live.com>
2018-07-20 17:14:48 -0700

    Add versioning information

---
+++ SpaceDefense/Assets/Scripts/Settings/Version.cs
@@ -0,0 +1,24 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Version.cs">
+//    Copyright (c) Yifei Xu .  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Assets.Scripts.Settings
+{
+    /// <summary>
+    /// All version related information
+    /// </summary>
+    public static class Version
+    {
+        public const int MajorVersion = 0;
+        public const int MinorVersion = 1;
+        public const int PointRelease = 0;
+        public const string GitHash = "";
+
+        public static string GetReleaseVersion()
+        {
+            return "v" + MajorVersion + "." + MinorVersion + "." + PointRelease + "-" + GitHash;
+        }
+    }
+}
---
+++ SpaceDefense/Assets/Scripts/UI/VersionFooter.cs
@@ -0,0 +1,29 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="VersionFooter.cs">
+//    Copyright (c) Yifei Xu .  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Assets.Scripts.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// An UI elements that shows the version information
+    /// </summary>
+    public class VersionFooter :  MonoBehaviour
+    {
+        /// <summary>
+        /// Used for initialization
+        /// </summary>
+        protected void Start()
+        {
+            this.GetComponent<Text>().text = Settings.Version.GetReleaseVersion();
+        }
+    }
+}
