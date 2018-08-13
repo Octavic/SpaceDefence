41c04189c30e58314474e668a7a3f672b6fff82c
Octavic <yifeixu@live.com>
2018-08-13 09:21:27 -0700

    Add spawn path indicators

--- SpaceDefense/Assets/Scripts/SpawnManager.cs
+++ SpaceDefense/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,7 @@ namespace Assets.Scripts
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using UnityEngine;
-    using Settings;
     using Enemies;
     
     [Serializable]
