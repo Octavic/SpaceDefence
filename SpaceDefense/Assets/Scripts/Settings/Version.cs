1fabde6c73d825151b66de71f0ed459e91852dee
Octavic <yifeixu@live.com>
2018-09-27 09:22:23 -0700

    Add git hash

--- SpaceDefense/Assets/Scripts/Settings/Version.cs
+++ SpaceDefense/Assets/Scripts/Settings/Version.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts.Settings
         public const int MajorVersion = 1;
         public const int MinorVersion = 0;
         public const int PointRelease = 0;
-        public const string GitHash = "";
+        public const string GitHash = "196fca0";
 
         public static string GetReleaseVersion()
         {
