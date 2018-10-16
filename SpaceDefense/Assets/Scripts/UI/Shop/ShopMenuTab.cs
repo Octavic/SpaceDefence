ca91effb7e98c96465a17144d353815211cc92be
Octavic <yifeixu@live.com>
2018-10-16 00:42:16 -0700

    Add shop menu tabs

---
+++ SpaceDefense/Assets/Scripts/UI/Shop/ShopMenuTab.cs
@@ -0,0 +1,77 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="SaveManager.cs">
+//    Copyright (c) Yifei Xu .  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Assets.Scripts.UI.Shop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Defines the shop menu tab that can be moved left => right
+    /// </summary>
+    public class ShopMenuTab : MonoBehaviour
+    {
+        /// <summary>
+        /// Width of the tab, also how much to move by
+        /// </summary>
+        public float TabWidth;
+
+        public Button LeftButton;
+        public Button RightButton;
+
+        /// <summary>
+        /// All of the sub contents
+        /// </summary>
+        private List<ShopMenuTabContent> _contents;
+
+        /// <summary>
+        /// Index of the content that's currently being shown
+        /// </summary>
+        private int _currentShowingIndex;
+
+        /// <summary>
+        /// Used for initialization
+        /// </summary>
+        protected void Start()
+        {
+            this._contents = this.transform.GetComponentsInChildren<ShopMenuTabContent>().ToList();
+            if(this._contents.Count == 0)
+            {
+                Debug.LogError("No content found for shop menu tab " + this.gameObject.name);
+            }
+
+            if(this._contents.Count == 1)
+            {
+                this.LeftButton.interactable = false;
+                this.RightButton.interactable = false;
+            }
+
+            this._currentShowingIndex = 0;
+        }
+        
+        public void Shift(int offset)
+        {
+            if(offset != 1 && offset != -1)
+            {
+                Debug.LogError("Shouldn't move more/less than 1");
+            }
+
+            var currentContent = this._contents[this._currentShowingIndex];
+            this._currentShowingIndex += offset;
+            this._currentShowingIndex = this._currentShowingIndex % this._contents.Count;
+            var newContent = this._contents[this._currentShowingIndex];
+
+            currentContent.LerpTo(new Vector3(this.TabWidth * offset, 0));
+            newContent.transform.localPosition = new Vector3(-this.TabWidth * offset, 0);
+            newContent.LerpTo(new Vector3(0, 0));
+
+        }
+    }
+}
---
+++ SpaceDefense/Assets/Scripts/UI/Shop/ShopMenuTabContent.cs
@@ -0,0 +1,61 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ShopMenuTabContent.cs">
+//    Copyright (c) Yifei Xu .  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Assets.Scripts.UI.Shop
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    /// <summary>
+    /// Defines a group of buttons in the shop menu
+    /// </summary>
+    public class ShopMenuTabContent : MonoBehaviour
+    {
+        /// <summary>
+        /// If the content is still moving
+        /// </summary>
+        private bool _isLerping;
+
+        /// <summary>
+        /// Where to lerp to
+        /// </summary>
+        private Vector3 _destination;
+
+        /// <summary>
+        /// Moves the content to the destination
+        /// </summary>
+        /// <param name="localDestination">Destination in local position</param>
+        public void LerpTo(Vector3 localDestination)
+        {
+            this._isLerping = true;
+            this._destination = localDestination;
+            this.StartCoroutine(this.StopLerpingAfter());
+        }
+
+        /// <summary>
+        /// Called at set intervals
+        /// </summary>
+        protected void FixedUpdate()
+        {
+            if(this._isLerping)
+            {
+                var oldPos = this.transform.localPosition;
+                this.transform.localPosition = Vector3.Lerp(oldPos, this._destination, 0.4f);
+            }
+        }
+
+        private IEnumerator StopLerpingAfter()
+        {
+            yield return new WaitForSeconds(1.5f);
+            this._isLerping = false;
+            this.transform.localPosition = this._destination;
+        }
+    }
+}
